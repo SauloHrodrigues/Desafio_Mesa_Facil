98cf2ae0c097d74f74ef6a869126d3483a2589e8
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-10-13 17:11:58 -0300

    feat: Configuração básica do projeto.

---
+++ src/main/java/com/db/desafio/Mesa_Facil/MesaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.db.desafio.Mesa_Facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/desafio/Mesa_Facil/MesaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.desafio.Mesa_Facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
