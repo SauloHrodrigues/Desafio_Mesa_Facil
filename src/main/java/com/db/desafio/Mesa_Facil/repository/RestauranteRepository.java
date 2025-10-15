b760ef37dc2767dc05d30b7a782338f6ee615827
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-10-15 15:39:09 -0300

    feat - Repository do restaurante: Inserindo a repository;

--- src/main/java/com/db/desafio/Mesa_Facil/repository/RestauranteRepository.java
+++ src/main/java/com/db/desafio/Mesa_Facil/repository/RestauranteRepository.java
@@ -1,9 +1,12 @@
 package com.db.desafio.Mesa_Facil.repository;
 
 import com.db.desafio.Mesa_Facil.model.Restaurante;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
+
+    Optional<Restaurante> findByNomeIgnoreCase(String nome);
 }
