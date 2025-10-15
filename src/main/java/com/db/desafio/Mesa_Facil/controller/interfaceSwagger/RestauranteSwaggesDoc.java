53b8d491c7e8207ce9c744e098aa46200bd5287a
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-10-15 15:46:33 -0300

    feat - Restaurante Swagger: criando interaface swagger para a controller de restaurantes;

---
+++ src/main/java/com/db/desafio/Mesa_Facil/controller/interfaceSwagger/RestauranteSwaggesDoc.java
@@ -0,0 +1,21 @@
+package com.db.desafio.Mesa_Facil.controller.interfaceSwagger;
+
+import com.db.desafio.Mesa_Facil.dtos.restaurante.NovoRestauranteDto;
+import com.db.desafio.Mesa_Facil.dtos.restaurante.RestauranteResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Restaturante",description = "Endpoints para gerenciar restaurantes")
+public interface RestauranteSwaggesDoc {
+
+    @Operation(summary = "Receber voto")
+    @ApiResponse(responseCode = "201", description = "Receber um voto com sucesso.",
+            content = @Content(schema = @Schema(implementation = NovoRestauranteDto.class)))
+    ResponseEntity<RestauranteResponseDto> cadastrar(@Valid @RequestBody NovoRestauranteDto dto);
+}
